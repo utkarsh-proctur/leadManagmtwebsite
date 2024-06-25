c6c05265dfb5193d0be0aad60bdf4ff328cf99bb
utkarsh-proctur <utkarsh@proctur.com>
2024-06-25 13:43:14 +0530

    changes done

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -4,20 +4,20 @@ import img1 from "../images/hritherosectionimg.png";
 import img2 from "../images/hritgroupofinstitutions.png";
 import img3 from "../images/hritherosectionimg.png";
 import img4 from "../images/hritgroupofinstitutions.png";
-import admissionImg from "../images/admissionlinkimg.png"; 
-
+import admissionImg from "../images/admissionlinkimg.png";
+import Admissionform from "../AdmissionForm/AdmissionForm";
 
 const HeroSection = () => {
   const [currentImage, setCurrentImage] = useState(img1);
   const [dots, setDots] = useState(Array(4).fill(false));
+  const [showForm, setShowForm] = useState(true);
   const images = [img1, img2, img3, img4];
-
 
   useEffect(() => {
     const interval = setInterval(() => {
       const nextImageIndex = (images.indexOf(currentImage) + 1) % images.length;
       setCurrentImage(images[nextImageIndex]);
-    }, 5000); 
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [currentImage]);
@@ -26,14 +26,31 @@ const HeroSection = () => {
     setCurrentImage(images[index]);
   };
 
-
-
   return (
     <section className="hero-section">
-      <img src={currentImage} alt="Hero Image" className="hero-image" />
-      <div className="hero-content">
-      <img src={admissionImg} alt="Admission Image"  className="admission-img" />
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          right: 0,
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src={admissionImg}
+          alt="Admission Image"
+          className="admission-img"
+          onClick={() => {
+            setShowForm(!showForm);
+          }}
+        />
+
+        {showForm && <Admissionform />}
       </div>
+
+      <img src={currentImage} alt="Hero Image" className="hero-image" />
+
       <div className="dots">
         {dots.map((dot, index) => (
           <span
